e0ea0aa783a0ef8fe91494ce461b543df4399ea0
ShaileshShetty <shetty.shetty15@gmail.cpm>
2020-01-16 11:23:58 +0000

    UI Initial Push

--- models/shop.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var Shop = mongoose.model("Shop", {
-  name: {type: String},
-  address: {type: String},
-  registration_no: {type: String}
-});
-
-module.exports = { Shop }
